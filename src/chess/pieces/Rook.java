b45dcbe4fe6868875902427b147960f098216eb7
Rafael Ceconi <rafaelvce@gmail.com>
2020-05-09 18:18:58 -0300

    Printing possible moves

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -4,7 +4,6 @@ import boardgame.Board;
 import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
-import sun.tools.tree.PostIncExpression;
 
 public class Rook extends ChessPiece{
 
